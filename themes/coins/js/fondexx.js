43cdf3c9dd8db9706e3bc83d588673d614b906a3
ArialBlack <andreyturik@gmail.com>
2016-09-16 15:56:05 +0300

    init

---
+++ themes/coins/js/fondexx.js
@@ -0,0 +1,18 @@
+(function ($) {
+    $(function() {
+        
+        /*function disableBootstrapMenuDropdown() {
+            $('#navbar .navbar-nav .dropdown-toggle').data('toggle', '').removeAttr('data-toggle');;
+        }*/
+        
+        /////////////////////////////////////
+        /////////////////////////////////////
+        /////////////////////////////////////
+        $( document ).ready(function() {  
+            // let's disable Bootstrap dropdown beh. for primary nav. We don't need it at all
+            //disableBootstrapMenuDropdown(); 
+        });
+
+
+    });
+}(jQuery));
