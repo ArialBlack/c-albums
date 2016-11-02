ee11597c2ada14fe92823ae124135b568ed532fd
ArialBlack <andreyturik@gmail.com>
2016-11-02 15:42:40 +0200

    add init catalog js behv.

--- themes/lux/js/init.js
+++ themes/lux/js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
 
+
       $(document).ready(function() {
           $('.button-collapse').sideNav({
               menuWidth: 300, // Default is 240
@@ -17,6 +18,41 @@
               delay: 50,
               html: true
           });
+
+          $('#catalog-tree li a').click(function( event ) {
+              $link = $(this);
+              $next =  $link.next();
+              //console.log($next);
+              //
+              if($next.hasClass('item-list')) {
+                 // $('.item-list').removeClass('open');
+                  $insertlink = $link.clone().prop('outerHTML');
+                  $link.addClass('hide');
+                  $link.parent('li').siblings().addClass('hide');
+                  $navhtml = $('#catalog-nav').html();
+                  $('#catalog-nav').html($navhtml + $insertlink);
+                  $next.addClass('open');
+                  event.preventDefault();
+              }
+          });
+
+          $(document).on('click', $('#catalog-nav a'), function( event ) {
+
+              $navlink = $(event.target);
+              //console.log($navlink.parent('#catalog-nav').length);
+              if ($navlink.parent('#catalog-nav').length > 0 ) {
+                  $selector = $('.' + $navlink.attr('class'));
+                  //console.log($selector);
+                  $chainnedLink =  $('#catalog-tree').find($selector);
+                  $chainnedLink.removeClass('hide');
+                  $('#catalog-nav').find($selector).remove();
+                  event.preventDefault();
+
+                  //если первый то показыать корень
+              }
+
+          });
+
       });
 
       $(document).on('CToolsAttachBehaviors', function () {
@@ -28,6 +64,8 @@
           $myselect = $('select');//.not('.field-widget-taxonomy-shs select');
           $myselect.material_select();
       });
+
+
 
       $('#edit-field-sell-item input:checkbox').change(function () {
           var check = $(this).prop('checked'),
